573fd2d80548bfbaa228bf0d59e2fa6d7185a322
audh25-lab <audh25@gmail.com>
2025-12-16 06:04:29 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,24 @@
 const CACHE = "barefeetmv-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json"
+      ])
+    )
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(
-      res => res || fetch(e.request)
-    )
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
